3d77e81210391eb15aa8afb70816e69d7a0f28db
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-01-02 17:48:19 +0000

    initial setup for switching JSON to psql db

---
+++ db/index.js
@@ -0,0 +1,7 @@
+const pgp = require("pg-promise")();
+
+const config = require("../config");
+
+const db = pgp(config);
+
+module.exports = db;
---
+++ seed.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS jokes;
+CREATE DATABASE jokes;
+
+\c jokes
+
+CREATE TABLE Jokes (
+  joke_id SERIAL,
+  joke VARCHAR,
+  author_id INT,
+  score INT,
+  PRIMARY KEY (joke_id),
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Authors (
+  author_id SERIAL,
+  author_name VARCHAR,
+  PRIMARY KEY (author_id)
+);
