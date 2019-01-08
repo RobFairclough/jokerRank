98b5ccaf23f8894a9f9d095b613e18f09dd0244d
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-01-08 08:10:31 +0000

    aws test 2

--- listen.js
+++ listen.js
@@ -1,7 +1,18 @@
 const app = require("./mainapp");
 
-app.listen(9090, () => {
-  console.log("listening");
+// normalise a port into a number, string or false
+const normalisePort = val => {
+  const port = parseInt(val, 10);
+  return isNaN(port) ? val : port >= 0 ? port : false;
+};
+const port = normalisePort(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
+
+var DB_CONFIG = process.env.DB_CONFIGURATION;
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 module.exports = app;
